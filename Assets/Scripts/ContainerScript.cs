c71a937f05928b010d0a027d9973e08e3ccce999
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-12-20 09:44:33 +0530

    Changed from spaces to tabs

--- Assets/Scripts/ContainerScript.cs
+++ Assets/Scripts/ContainerScript.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class ContainerScript : MonoBehaviour {
 
-    [SerializeField] private Outline outline;
-    [SerializeField] private Renderer renderer;
-    
-    [HideInInspector] public bool isCollapsed = false;
-    [HideInInspector] public HashSet<int> allowedTiles = new HashSet<int>();
+	[SerializeField] private Outline outline;
+	[SerializeField] private Renderer renderer;
+	
+	[HideInInspector] public bool isCollapsed = false;
+	[HideInInspector] public HashSet<int> allowedTiles = new HashSet<int>();
 
-    void Start(){
-        outline.enabled = false;
-        renderer.enabled = true;
-    }
+	void Start(){
+		outline.enabled = false;
+		renderer.enabled = true;
+	}
 
-    void OnMouseEnter(){
-        outline.enabled = true;
-    }
-    void OnMouseExit(){
-        outline.enabled = false;
-    }
+	void OnMouseEnter(){
+		outline.enabled = true;
+	}
+	void OnMouseExit(){
+		outline.enabled = false;
+	}
 
-    public void Collapse(GameObject[] tiles, int tileIndex){
-        // Disable rendering for this container
-        renderer.enabled = false;
-        // Set isCollapsed = true
-        isCollapsed = true;
-        // Spawn the new tile
-        Instantiate(tiles[tileIndex], transform.position, Quaternion.identity, transform.parent);
-    }
+	public void Collapse(GameObject[] tiles, int tileIndex){
+		// Disable rendering for this container
+		renderer.enabled = false;
+		// Set isCollapsed = true
+		isCollapsed = true;
+		// Spawn the new tile
+		Instantiate(tiles[tileIndex], transform.position, Quaternion.identity, transform.parent);
+	}
 }
